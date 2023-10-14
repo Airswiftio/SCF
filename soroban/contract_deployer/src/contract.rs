6e599acc4fb8ce830158a533bfbd4eb021c2d740
c0linw <41310013+c0linw@users.noreply.github.com>
2023-10-13 17:16:46 -0700

    Support calling initialization steps directly from deployer contract

--- soroban/contract_deployer/src/contract.rs
+++ soroban/contract_deployer/src/contract.rs
@@ -1,16 +1,22 @@
 //! This contract demonstrates a sample implementation of the Soroban token
 //! interface.
 use crate::admin::{has_administrator, read_administrator, write_administrator};
-use soroban_sdk::{contract, contractimpl, symbol_short, Address,  Env,  Bytes, BytesN, String, IntoVal, Val, Vec};
-
-
+use soroban_sdk::{
+    contract, contractimpl, symbol_short, vec, Address, Bytes, BytesN, Env, IntoVal, String,
+    Symbol, Val, Vec,
+};
 
 pub trait DeployerTrait {
     fn initialize(e: Env, admin: Address);
-    fn DeployContract(e: Env, token_wasm_hash: BytesN<32>,  salt: BytesN<32> )-> Address;
-
+    fn DeployContract(
+        e: Env,
+        deployer: Address,
+        token_wasm_hash: BytesN<32>,
+        salt: BytesN<32>,
+        init_fn_list: Vec<Symbol>,
+        init_args_list: Vec<Vec<Val>>,
+    ) -> Address;
 }
-
 
 #[contract]
 pub struct Deployer;
@@ -18,30 +24,40 @@ pub struct Deployer;
 #[contractimpl]
 impl DeployerTrait for Deployer {
     fn initialize(e: Env, admin: Address) {
-        
         if has_administrator(&e) {
             panic!("already initialized")
         }
         write_administrator(&e, &admin);
     }
 
-
-    fn DeployContract(e: Env, token_wasm_hash: BytesN<32>,  salt: BytesN<32> )-> Address {
-        
-        // let admin = read_administrator(&e);
-        // admin.require_auth();
+    fn DeployContract(
+        e: Env,
+        deployer: Address,
+        token_wasm_hash: BytesN<32>,
+        salt: BytesN<32>,
+        init_fn_list: Vec<Symbol>,
+        init_args_list: Vec<Vec<Val>>,
+    ) -> Address {
+        if deployer != e.current_contract_address() {
+            deployer.require_auth();
+        }
+        if (init_fn_list.len() != init_args_list.len()) {
+            panic!("init arguments length mismatch")
+        }
 
         let deployed_address = e
-        .deployer()
-        .with_address(e.current_contract_address(), salt)
-        .deploy(token_wasm_hash);
+            .deployer()
+            .with_address(e.current_contract_address(), salt)
+            .deploy(token_wasm_hash);
 
-        // let init_fn = symbol_short!("init");
+        for i in 0..(init_fn_list.len() - 1) {
+            let _res: Val = e.invoke_contract(
+                &deployed_address,
+                &init_fn_list.get(i).unwrap(),
+                init_args_list.get(i).unwrap(),
+            );
+        }
 
-
-        // let res: Val = e.invoke_contract(&deployed_address, &init_fn, init_args);
-        
-        (deployed_address)
+        deployed_address
     }
-
 }
