63c3984dae58ddd11d0bc14d885466906904e465
c0linw <41310013+c0linw@users.noreply.github.com>
2023-10-16 14:30:37 -0700

    contract_deployer: fix indexing bug in argument handling loop

--- soroban/contract_deployer/src/contract.rs
+++ soroban/contract_deployer/src/contract.rs
@@ -41,16 +41,16 @@ impl DeployerTrait for Deployer {
         if deployer != e.current_contract_address() {
             deployer.require_auth();
         }
-        if (init_fn_list.len() != init_args_list.len()) {
+        if init_fn_list.len() != init_args_list.len() {
             panic!("init arguments length mismatch")
         }
 
         let deployed_address = e
             .deployer()
-            .with_address(e.current_contract_address(), salt)
+            .with_address(deployer, salt)
             .deploy(token_wasm_hash);
 
-        for i in 0..(init_fn_list.len() - 1) {
+        for i in 0..init_fn_list.len() {
             let _res: Val = e.invoke_contract(
                 &deployed_address,
                 &init_fn_list.get(i).unwrap(),
