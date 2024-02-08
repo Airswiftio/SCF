a6e1c691d160a79922bc7a3e4d65be87446d0b8c
c0linw <41310013+c0linw@users.noreply.github.com>
2024-02-07 17:33:05 -0800

    feat(scripts): add deploy scripts and update bump scripts

--- soroban/scf_soroban/bump.sh
+++ soroban/scf_soroban/bump.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/SCF_contracts/soroban/scf_soroban/
-soroban contract bump --wasm target/wasm32-unknown-unknown/release/scf_soroban.wasm --network futurenet --source-account admin --durability persistent --ledgers-to-expire 3000000
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+soroban contract extend --id $(cat ./contract_address) --network $(cat ../network_name) --source-account admin --durability persistent --ledgers-to-extend 3000000
+soroban contract extend --wasm target/wasm32-unknown-unknown/release/scf_soroban.wasm --network $(cat ../network_name) --source-account admin --durability persistent --ledgers-to-extend 3000000
