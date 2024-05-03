7c2e396b98b13ebe49734681ff19acec2301101f
c0linw <41310013+c0linw@users.noreply.github.com>
2024-05-02 17:13:53 -0700

    fix(contract_deployer): remove unused code and implement test

--- soroban/contract_deployer/src/errors.rs
+++ soroban/contract_deployer/src/errors.rs
@@ -4,6 +4,5 @@ use soroban_sdk::contracterror;
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 #[repr(u32)]
 pub enum Error {
-    AlreadyInitialized = 1,
-    ArgumentLengthMismatch = 2,
+    ArgumentLengthMismatch = 1,
 }
