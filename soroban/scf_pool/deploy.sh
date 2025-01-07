c3138f2c5013feb567a56562660e34b40d2da2fe
c0linw <41310013+c0linw@users.noreply.github.com>
2025-01-07 11:49:31 -0800

    fix(scf_pool): update deploy script for constructor argument

--- soroban/scf_pool/deploy.sh
+++ soroban/scf_pool/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
-soroban contract deploy --network $(cat ../network_name) --source admin --wasm target/wasm32-unknown-unknown/release/pool.wasm | tee ./contract_address
+soroban contract deploy --network $(cat ../network_name) --source admin --wasm target/wasm32-unknown-unknown/release/pool.wasm -- --admin admin | tee ./contract_address
